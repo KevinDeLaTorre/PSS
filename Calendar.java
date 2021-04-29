a4311153ab1db4c54e6a61e2524c10b444b5a30a
Kevin De La Torre <kevindelatorre@live.com>
2021-04-29 16:32:11 -0700

    Created skeleton of Calendar class.

---
+++ Calendar.java
@@ -0,0 +1,46 @@
+import java.util.Vector;
+
+/**
+ * Calendar class is in charge of scheduling and managing tasks, it will also update the file after every task change.
+ * Because this class has a list of all the tasks it is also in charge of interfacing with the Report class to generate reports.
+ */
+public class Calendar {
+  private Vector<Task> listOfTasks;
+  private String filename;
+
+  public Calendar( String filename ) {
+    listOfTasks = new Vector<Task>();
+    this.filename = filename;
+  }
+
+  public boolean scheduleTask( Task newTask ) {
+
+  }
+
+  public boolean editTask( String taskName ) {
+
+  }
+
+  public boolean deleteTask( String taskName ) {
+
+  }
+
+  public Vector<Task> getAllTasks() {
+    return listOfTasks;
+  }
+
+  public boolean updateFile() {
+  }
+
+  public boolean writePartSchedule( String filename, int date, int frequency ) {
+
+  }
+
+  public String generateFullReport() {
+
+  }
+
+  public String generatePartReport() {
+
+  }
+}
