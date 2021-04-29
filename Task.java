1d1d6bf1e5884d7a235564513cec6f0e22bc7164
Kevin De La Torre <kevindelatorre@live.com>
2021-04-28 23:20:45 -0700

    Added a list for valid tasks and a type check in the constructor.

--- Task.java
+++ Task.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * This class is the Super class of all the task types it holds the basic information and methods that all the tasks would need to have.
  */
 public class Task {
+  // List of general types all tasks can do
+  private final String[] typesArray = { "Class", "Study", "Sleep", "Exercise", "Work", "Meal" };
+  private final List<String> types = new ArrayList<String>(Arrays.asList( typesArray ));
+
   private String taskName;
   private String type;
   private double startTime;
@@ -19,10 +27,16 @@ public class Task {
    */
   public Task( String taskName, String type, double startTime, int startDate, double duration ) {
     this.taskName = taskName;
-    this.type = type;
     this.startTime = startTime;
     this.startDate = startDate;
     this.duration = duration;
+
+    // Checks to make sure a valid type is given, makes the type "Error" if not
+    if ( types.contains(type) == false ) {
+      this.type = "Error";
+    } else {
+      this.type = type;
+    }
   }
 
   /**
