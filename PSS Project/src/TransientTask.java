6a1b76c0b5e533ecd8515a4cdb443d47b5ed3094
dkathiriya360 <83369757+dkathiriya360@users.noreply.github.com>
2021-05-04 19:43:41 -0700

    added transient task

---
+++ PSS Project/src/TransientTask.java
@@ -0,0 +1,14 @@
+
+public class TransientTask extends Task{
+	
+
+	public TransientTask(String taskName, String type, double startTime, int startDate, double duration)
+			throws RestrictionCheckFailedException {
+		super(taskName, type, startTime, startDate, duration);
+		
+	}
+	// TODO restriction check
+	
+	
+
+}
