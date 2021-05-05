e8bd6b5555703ca35bbbd713bf2750479cac0080
Dharam <83369757+dkathiriya360@users.noreply.github.com>
2021-05-04 18:26:06 -0700

    Update RecurringTask.java

--- RecurringTask.java
+++ RecurringTask.java
@@ -1,1 +1,16 @@
 
+public class RecurringTask extends Task{
+	private int startDate;
+	private int endDate;
+	private int frequency;
+	
+	public RecurringTask(String taskName, String type, double startTime, int startDate, double duration, int sDate, int eDate, int freq) {
+		super(taskName, type, startTime, startDate, duration);
+		this.startDate = sDate;
+		endDate = eDate;
+		frequency = freq;
+	}
+
+}
+
+
