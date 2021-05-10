f2b085bc37ac57b46a9e31e6d7965edafe2464e8
dkathiriya360 <83369757+dkathiriya360@users.noreply.github.com>
2021-05-10 02:38:50 -0700

    added task types

--- TransientTask.java
+++ TransientTask.java
@@ -1,6 +1,8 @@
 
 public class TransientTask extends Task {
-
+	// transient task types
+	private final String[] types = {"Visit", "Shopping", "Appointment"};
+	
 	public TransientTask(String taskName, String type, double startTime, int startDate, double duration) throws RestrictionCheckFailedException {
 		super(taskName, type, startTime, startDate, duration);
 		
@@ -9,7 +11,7 @@ public class TransientTask extends Task {
 		}
 	}
 	
-	public boolean checkExtraRestrictions()
+	public boolean checkRestrictions()
 	{
 		// TODO: check additional restrictions
 		return false;
