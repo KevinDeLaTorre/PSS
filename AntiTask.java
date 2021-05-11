046123126c6d1e77934c24952c12090bdc2d334f
dkathiriya360 <83369757+dkathiriya360@users.noreply.github.com>
2021-05-10 21:39:25 -0700

    added checkRestriction to validate taskType

--- AntiTask.java
+++ AntiTask.java
@@ -1,20 +1,43 @@
 
 public class AntiTask extends Task {
- 
+	private final String[] types = {"Cancellation"}; // only task type
+	
 	public AntiTask(String taskName, String type, double startTime, int startDate, double duration) throws RestrictionCheckFailedException {
 		super(taskName, type, startTime, startDate, duration);
 		
-		if(checkRestrictions() == false) {
+		if(checkRestrictions(super.getType()) == false) {
 		      throw new RestrictionCheckFailedException( "Restriction check failed." );
 		}
 	}
 
-	public boolean checkRestrictions()
+	/**
+	 * Checks if the inputs are valid
+	 * @param type the type of task
+	 * @return true if the task is anti-task
+	 */
+	public boolean checkRestrictions(String type)
 	{
-		// TODO: check additional restrictions
+		boolean antiType = checkType(type); 
+		if (antiType == true)
+			return true; 
 		return false;
 	}
-
-	// To Do: add additional methods 
+	
+	/**
+	   * Checks to make sure type input is a valid one.
+	   * @param type  Type of task to check i.e. Class, Study, Sleep...
+	   * @return contains Returns true if type is valid
+	   */
+	  public boolean checkType( String type ) {
+	    // Checks to make sure a valid type is given, makes the type "Error" if not
+	    boolean contains = false;
+	    for ( int i = 0; i < types.length; i++ ) {
+	      if ( type == types[i] ) {
+	        contains = true;
+	        break;
+	      }
+	    }
+	    return contains;
+	  }
 	
 }
