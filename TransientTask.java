3a5506de0d975d65cee42592e0bb5a50a03c6cae
dkathiriya360 <83369757+dkathiriya360@users.noreply.github.com>
2021-05-10 21:36:51 -0700

    added restrictions

--- TransientTask.java
+++ TransientTask.java
@@ -6,16 +6,38 @@ public class TransientTask extends Task {
 	public TransientTask(String taskName, String type, double startTime, int startDate, double duration) throws RestrictionCheckFailedException {
 		super(taskName, type, startTime, startDate, duration);
 		
-		if(checkRestrictions() == false) {
+		if(checkRestrictions(super.getType()) == false) {
 		      throw new RestrictionCheckFailedException( "Restriction check failed." );
 		}
 	}
 	
-	public boolean checkRestrictions()
+	/**
+	 * Checks if the input value (task type) is valid
+	 * @param type the type of task
+	 * @return true if the type is transient
+	 */
+	public boolean checkRestrictions(String type)
 	{
-		// TODO: check additional restrictions
+		boolean transientType = checkType(type); // check whether the taskType is transient
+		if (transientType == true)
+			return true; 
 		return false;
 	}
-
-	// To Do: add additional methods 
+	
+	/**
+	   * Checks to make sure type input is a valid one.
+	   * @param type  Type of task to check i.e. Class, Study, Sleep...
+	   * @return contains Returns true if type is valid
+	   */
+	  public boolean checkType( String type ) {
+	    // Checks to make sure a valid type is given, makes the type "Error" if not
+	    boolean contains = false;
+	    for ( int i = 0; i < types.length; i++ ) {
+	      if ( type == types[i] ) {
+	        contains = true;
+	        break;
+	      }
+	    }
+	    return contains;
+	  }
 }
