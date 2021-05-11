0847f845d3e010d176d0012e1b50c58556700da3
Kevin De La Torre <kevindelatorre@live.com>
2021-05-11 00:34:01 -0700

    Added semi-working schedule task, added editTask, fixed method of searching through list, added getNextDay.

--- Calendar.java
+++ Calendar.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collections;
+import java.util.Set;
 /**
  * Calendar class is in charge of scheduling and managing tasks, it will also update the file after every task change.
  * Because this class has a list of all the tasks it is also in charge of interfacing with the Report class to generate reports.
@@ -8,10 +9,12 @@ import java.util.Collections;
 public class Calendar {
   private HashMap<Integer, ArrayList<Task>> _listOfTasks;
   private DataFile _file;
+  private Set<Integer> _keys;
 
   public Calendar( DataFile file ) {
     _listOfTasks = new HashMap<Integer, ArrayList<Task>>();
     _file = file;
+    _keys = _listOfTasks.keySet();
   }
 
   /**
@@ -21,13 +24,21 @@ public class Calendar {
    */
   public boolean scheduleTask( Task newTask ) {
     // Check for name uniqueness
-    if ( contains( newTask.getName() ) ) {
-      return false;
+    try {
+      getTask( newTask.getName() );
+      return false; // If getTask doesn't throw an error it means a task was found therefore newTask is not unique so return false.
+    } catch ( TaskNotFoundException e ) {}
+
+    // If first time adding to date, create key in table
+    if ( !_keys.contains(newTask.getStartDate() ) ) { 
+      _listOfTasks.put( newTask.getStartDate(), new ArrayList<Task>() );
     }
 
-      
+    // TODO: Check if time overlap
 
-    sortList();
+    _listOfTasks.get( newTask.getStartDate() ).add( newTask ); // Add task to listOfTasks
+    Collections.sort(_listOfTasks.get( newTask.getStartDate())); // Sort arraylist by date/time after adding new task in
+    _keys = _listOfTasks.keySet(); // Update _keys
     return true;
   }
 
@@ -39,18 +50,24 @@ public class Calendar {
    */
   public Task getTask( String taskName ) throws TaskNotFoundException {
     // Search list for taskname
-    for ( int i = 0; i < _listOfTasks.size(); i++ ) { // Search all keys in hash table
-      for ( int j = 0; j < _listOfTasks.get( i ).size(); j++ ) { // Search through arraylist of key
-        if ( _listOfTasks.get( i ).get( j ).getName() == taskName ) {
-          return _listOfTasks.get( i ).get( j );
+    for ( int key : _keys ) { // Search all keys in hash table
+      for ( int j = 0; j < _listOfTasks.get( key ).size(); j++ ) { // Search through arraylist of key
+        if ( _listOfTasks.get( key ).get( j ).getName() == taskName ) { // If taskname is found return task
+          return _listOfTasks.get( key ).get( j );
         }
       }
     }
     throw new TaskNotFoundException( "Task not found." );
   }
 
-  public boolean editTask( String taskName ) {
-    return false;
+  /**
+   * Give this method a task with the same name as one already in the system and it'll replace it with the updated one.
+   * @param updatedTask Updated task of the one you want to edit, has to have same name of task in listOfTasks.
+   * @return success Returns true if scheduled task successfully
+   */
+  public boolean editTask( Task updatedTask ) {
+    deleteTask( updatedTask.getName() );
+    return scheduleTask( updatedTask );
   }
 
   /**
@@ -59,10 +76,10 @@ public class Calendar {
    * @return Returns true/false depending on if task was successfully deleted or wasn't found.
    */
   public boolean deleteTask( String taskName ) {
-    for ( int i = 0; i < _listOfTasks.size(); i++ ) { // Search all keys in hash table
-      for ( int j = 0; j < _listOfTasks.get( i ).size(); j++ ) { // Search through arraylist of key
-        if ( _listOfTasks.get( i ).get( j ).getName() == taskName ) {
-          _listOfTasks.get( i ).remove( _listOfTasks.get( i ).get( j ) ); // If task found delete 
+    for ( int key : _keys ) { // Search all keys in listOfTasks
+      for ( int j = 0; j < _listOfTasks.get( key ).size(); j++ ) { // Search through arraylist of key
+        if ( _listOfTasks.get( key ).get( j ).getName() == taskName ) {
+          _listOfTasks.get( key ).remove( _listOfTasks.get( key ).get( j ) ); // If task found delete 
           return true;
         }
       }
@@ -72,24 +89,41 @@ public class Calendar {
 
   public ArrayList<Task> getAllTasks() {
     ArrayList<Task> tmpList = new ArrayList<Task>();
-    for ( int i = 0; i < _listOfTasks.size(); i++ ) {
-      for ( int j = 0; j < _listOfTasks.get( i ).size(); j++ ) {
-        tmpList.add( _listOfTasks.get( i ).get( j ) );
+    for ( int key : _keys ) { // Go through entire hashtable and get an arraylist of all tasks
+      for ( int j = 0; j < _listOfTasks.get( key ).size(); j++ ) {
+        tmpList.add( _listOfTasks.get( key ).get( j ) );
       }
     }
+    Collections.sort( tmpList ); // Sorts all the arraylists by date/time
+    return tmpList;
+  }
+
+  public ArrayList<Task> getPartTasks( int startDate, int duration ) {
+    ArrayList<Task> tmpList = new ArrayList<Task>();
+    int date = startDate;
+    for ( int i = 0; i < duration; i++ ) {
+      if ( _keys.contains( date ) ) { // If date in listOfTasks get all tasks from that date
+        for ( int j = 0; j < _listOfTasks.get( date ).size(); j++ ) { // Search through all of arraylist of given date
+          tmpList.add( _listOfTasks.get( date ).get( j ) );
+        }
+      }
+      date = getNextDay( date );
+    }
+
     return tmpList;
   }
 
   public boolean updateFile() {
-    if ( _file.updateTasks( _listOfTasks ) ) {
+    if ( _file.updateTasks( getAllTasks() ) ) {
       return _file.writeToFile();
     } else {
       return false;
     }
   }
 
-  public boolean writePartSchedule( String filename, int date, int frequency ) {
-    DataFile tmpFile = new DataFile( filename );
+  public boolean writePartSchedule( String filename, int date, int duration ) {
+    ArrayList<Task> tasks = getPartTasks( date, duration );
+    DataFile tmpFile = new DataFile( filename, tasks );
     return tmpFile.writeToFile();
   }
 
@@ -103,23 +137,55 @@ public class Calendar {
     return "PARTIAL REPORT GOES HERE";
   }
 
+  private int getNextDay( int date ) {
+    String tmpString = String.valueOf( date ); // Date should be in format YYYYMMDD
+    int year = Integer.parseInt(tmpString.substring(0, 3)); // YYYY
+    int month = Integer.parseInt(tmpString.substring(4, 5)); // MM
+    int day = Integer.parseInt(tmpString.substring(6, 7)); // DD
+    
+    tmpString = "";
+    boolean changeMonth = false;
+
+    if ( month == 12 && day == 31 ) { // In case of a new years switch reset month and day to 1 and increase year by 1
+      tmpString += String.valueOf(year+1) + "0101";
+    } else if ( month == 2 && day == 28 ) { // If february and last day change month at 28
+      changeMonth = true;
+    } else if ( day == 30 && ( month == 4 || month == 6 || month == 9 || month == 11 ) ) { // If in last day of 30 day month change month
+      changeMonth = true;
+    } else if ( day == 31 ) { // if any other month and current day is 31
+      changeMonth = true;
+    }
+
+    if ( changeMonth ) { // If there is a month change, increase month by 1 and reset day to 1
+      month += 1;
+      day = 1;
+    } else {
+      day += 1; // If there isn't a month change then just increase day by 1 and return.
+    }
+
+    String tmpMonth = "";
+    if ( month < 10 ) { // If month is below 10, in order to fit the YYYYMMDD format we need a 0 in front of the integer
+      tmpMonth = "0" + String.valueOf( month );
+    } else {
+      tmpMonth = String.valueOf( month );
+    }
+
+    String tmpDay = "";
+    if ( day < 10 ) { // If day is below 10, in order to fit the YYYYMMDD format we need a 0 in front of the integer
+
+      tmpDay = "0" + String.valueOf( day );
+    } else {
+      tmpDay = String.valueOf( day );
+    }
+
+    tmpString += String.valueOf(year) + tmpMonth + tmpDay;
+
+    return Integer.parseInt( tmpString );
+  }
+
   private void sortList() {
     for ( int i = 0; i < _listOfTasks.size(); i++ ) {
       Collections.sort(_listOfTasks.get( i ));
-    }
-  }
-
-  /**
-   * Helper class that checks if the list of tasks contains a task searched by name.
-   * @param name
-   * @return result Returns whether the list of tasks contains the task or not.
-   */
-  private boolean contains( String name ) {
-    try { 
-      getTask( name );
-      return true;
-    } catch ( TaskNotFoundException e ) {
-      return false;
     }
   }
 }
