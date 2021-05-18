e2978a9416ab553a7c0076d617c5faf2fd8f3eca
Kevin De La Torre <kevindelatorre@live.com>
2021-05-18 00:52:44 -0700

    Changed checkRestriction a bit.

--- TransientTask.java
+++ TransientTask.java
@@ -7,7 +7,7 @@ public class TransientTask extends Task {
 	public TransientTask(String taskName, String type, double startTime, int startDate, double duration) throws RestrictionCheckFailedException {
 		super(taskName, type, startTime, startDate, duration);
 		
-		if(checkRestrictions(super.getType()) == false) {
+		if(checkRestrictions( type ) == false) {
 		      throw new RestrictionCheckFailedException( "Restriction check failed." );
 		}
 	}
